0c3348fa9facc533231494b7a386b1bd5b58b642
thienpn <1010thien1010@gmail.com>
2021-12-09 17:09:06 +0700

    Using uricodebin

--- deepstream-with-c-api/include/DS_usb_cam.hpp
+++ deepstream-with-c-api/include/DS_usb_cam.hpp
@@ -1,9 +1,53 @@
 #include <gst/gst.h>
 #include <glib.h>
 #include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <sys/time.h>
 #include <cuda_runtime_api.h>
-#include "gstnvdsmeta.h"
 
-static GstPadProbeReturn osd_sink_pad_buffer_probe (GstPad * pad, GstPadProbeInfo * info, gpointer u_data);
+#include "gstnvdsmeta.h"
+//#include "gstnvstreammeta.h"
+// #ifndef PLATFORM_TEGRA
+// #include "gst-nvmessage.h"
+// #endif
+
+#define MAX_DISPLAY_LEN 64
+
+#define PGIE_CLASS_ID_VEHICLE 0
+#define PGIE_CLASS_ID_PERSON 2
+
+/* By default, OSD process-mode is set to CPU_MODE. To change mode, set as:
+ * 1: GPU mode (for Tesla only)
+ * 2: HW mode (For Jetson only)
+ */
+#define OSD_PROCESS_MODE 0
+
+/* By default, OSD will not display text. To display text, change this to 1 */
+#define OSD_DISPLAY_TEXT 0
+
+/* The muxer output resolution must be set if the input streams will be of
+ * different resolution. The muxer will scale all the input frames to this
+ * resolution. */
+#define MUXER_OUTPUT_WIDTH 1920
+#define MUXER_OUTPUT_HEIGHT 1080
+
+/* Muxer batch formation timeout, for e.g. 40 millisec. Should ideally be set
+ * based on the fastest source's framerate. */
+#define MUXER_BATCH_TIMEOUT_USEC 40000
+
+#define TILED_OUTPUT_WIDTH 1280
+#define TILED_OUTPUT_HEIGHT 720
+
+/* NVIDIA Decoder source pad memory feature. This feature signifies that source
+ * pads having this capability will push GstBuffers containing cuda buffers. */
+#define GST_CAPS_FEATURES_NVMM "memory:NVMM"
+
+#define FPS_PRINT_INTERVAL 300
+
+static GstPadProbeReturn tiler_src_pad_buffer_probe (GstPad * pad, GstPadProbeInfo * info, gpointer u_data);
 static gboolean bus_call (GstBus * bus, GstMessage * msg, gpointer data);
+static void cb_newpad (GstElement * decodebin, GstPad * decoder_src_pad, gpointer data);
+static void decodebin_child_added (GstChildProxy * child_proxy, GObject * object, gchar * name, gpointer user_data);
+static GstElement *create_source_bin (guint index, gchar * uri);
 int deepstream_usb_cam (int argc, char *argv[]);
